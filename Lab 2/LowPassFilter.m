c91f32f64298272c85ce751c794fef14475ac1bb
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-01-19 22:43:44 +0600

    Create LowPassFilter.m

---
+++ Lab 2/LowPassFilter.m
@@ -0,0 +1,10 @@
+num = [0 0 1];
+den = [0 1 1];
+w = logspace(-4,4);
+h = freqs(num,den,w);
+f = w/(2*pi);
+mag = 20*log10(abs(h));
+semilogx(f,mag);
+grid on;
+xlabel('Frequency, Hz');
+ylabel('Gain, dB');
