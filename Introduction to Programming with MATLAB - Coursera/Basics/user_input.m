f6f51783468c6a2d3a836af6e6a5424718794113
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-08-03 12:00:08 +0600

    Create user_input.m

---
+++ Introduction to Programming with MATLAB - Coursera/Basics/user_input.m
@@ -0,0 +1,6 @@
+% Taking user input
+name = input('Enter your name: ', 's'); % 's' indicates string input
+age = input('Enter your age: '); % Numeric input by default
+
+% Printing output
+fprintf('Hello, %s! You are %d years old.\n', name, age);
